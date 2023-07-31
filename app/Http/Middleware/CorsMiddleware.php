87adfcf7d3c96335b6b415ddf5b93e4f06c5d2d7
Vincent Oti <mrvincentoti@gmail.com>
2023-07-31 11:19:29 +0100

    allowed all origing

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,23 +8,10 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        // Define allowed origins here
-        $allowedOrigins = [
-            'http://34.207.129.27',
-        ];
+        $response = $next($request);
 
-        $origin = $request->header('Origin');
-
-        if (in_array($origin, $allowedOrigins)) {
-            // Allow the origin if it's in the list
-            $response = $next($request);
-            $response->headers->set('Access-Control-Allow-Origin', $origin);
-        } else {
-            // Handle invalid origin, like sending a 403 Forbidden response
-            return response()->json(['error' => 'Invalid origin'], 403);
-        }
-
-        // Set other CORS headers
+        // Allow all origins (not recommended for production)
+        $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         $response->headers->set('Access-Control-Allow-Credentials', 'true');
