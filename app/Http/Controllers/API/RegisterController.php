9c3a5b86985c9f6fac2dcd182849a7d8bc2407bd
Wisdom George <wisgeorge.wg@gmail.com>
2024-01-08 12:12:18 +0100

    debugging the captcha in login logic

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -208,15 +208,6 @@ class RegisterController extends BaseController
      */
     public function login(Request $request): JsonResponse
     {
-        $captchaResponse = $request->input('g-recaptcha-response');
-        $captchaSecretKey = env("RECAPTCHA_SECRET_KEY");
-
-        // Send a POST request to Google reCAPTCHA verification endpoint
-        $verificationResponse = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
-            'secret' => $captchaSecretKey, 
-            'response' => $captchaResponse,
-        ]);
-        
         
         if(Auth::attempt(['username' => $request->username, 'password' => $request->password])){ 
             $user = Auth::user(); 
