3b8985d596fbeb57c85307226493a8100d84e17e
Wisdom George <wisgeorge.wg@gmail.com>
2024-02-26 10:10:09 +0100

    commented all extra routes

--- routes/api.php
+++ routes/api.php
@@ -39,7 +39,7 @@ Route::group(['middleware' => 'cors'], function () {
     Route::post('/users/resend-otp', [RegisterController::class, 'resendOtp']);
     Route::post('/users/change-password/{user_id}', [RegisterController::class,'changePassword']);
     Route::get('/users/get-info', [RegisterController::class,'getUserInfo']);
-
+    
     //Feedback and Rating
     Route::post('/feedback', [FeedbackController::class, 'submitFeedback']);
     Route::get('/feedback/user/{user_id}', [FeedbackController::class, 'getUserFeedback']);
@@ -87,43 +87,43 @@ Route::group(['middleware' => 'cors'], function () {
 });
 
 
-//Feedback and Rating
-Route::post('/feedback', [FeedbackController::class, 'submitFeedback']);
-Route::get('/feedback/user/{user_id}', [FeedbackController::class, 'getUserFeedback']);
-Route::get('/rating/average', [FeedbackController::class, 'getAverageFeedback']);
-//Wallet Payment
-Route::post('/wallet/create', [WalletController::class, 'createUsersWallet']);
-Route::post('/wallet/fund', [WalletController::class, 'fundWallet']);
-Route::get('/wallet/balance/{user_id}', [WalletController::class, 'getWalletBalance']);
-Route::post('/wallet/payment/{user_id}', [WalletController::class, 'makeWalletPayment']);
-// Transaction
-Route::post('/user/transaction', [TransactionController::class, 'createTransaction']);
-Route::get('/user/get-transaction/{user_id}', [TransactionController::class, 'getUserTransaction']);
-Route::get('/user/total-transaction/{user_id}', [TransactionController::class, 'getUserTotalTransaction']);
-Route::get('/user/monthly-transaction/{user_id}', [TransactionController::class, 'getUserMonthlyTransaction']);
-Route::get('/user/user-transaction-category/{category}/{user_id}', [TransactionController::class, 'getUserTransactionByCategory']);
-Route::post('/user/transaction-pin/create', [TransactionController::class, 'setTransactionPin']);
-Route::post('/user/transaction-pin/update', [TransactionController::class, 'updateTransactionPin']);
-Route::post('/user/transaction-pin/verify', [TransactionController::class, 'verifyTransactionPin']);
+// //Feedback and Rating
+// Route::post('/feedback', [FeedbackController::class, 'submitFeedback']);
+// Route::get('/feedback/user/{user_id}', [FeedbackController::class, 'getUserFeedback']);
+// Route::get('/rating/average', [FeedbackController::class, 'getAverageFeedback']);
+// //Wallet Payment
+// Route::post('/wallet/create', [WalletController::class, 'createUsersWallet']);
+// Route::post('/wallet/fund', [WalletController::class, 'fundWallet']);
+// Route::get('/wallet/balance/{user_id}', [WalletController::class, 'getWalletBalance']);
+// Route::post('/wallet/payment/{user_id}', [WalletController::class, 'makeWalletPayment']);
+// // Transaction
+// Route::post('/user/transaction', [TransactionController::class, 'createTransaction']);
+// Route::get('/user/get-transaction/{user_id}', [TransactionController::class, 'getUserTransaction']);
+// Route::get('/user/total-transaction/{user_id}', [TransactionController::class, 'getUserTotalTransaction']);
+// Route::get('/user/monthly-transaction/{user_id}', [TransactionController::class, 'getUserMonthlyTransaction']);
+// Route::get('/user/user-transaction-category/{category}/{user_id}', [TransactionController::class, 'getUserTransactionByCategory']);
+// Route::post('/user/transaction-pin/create', [TransactionController::class, 'setTransactionPin']);
+// Route::post('/user/transaction-pin/update', [TransactionController::class, 'updateTransactionPin']);
+// Route::post('/user/transaction-pin/verify', [TransactionController::class, 'verifyTransactionPin']);
 
-Route::post('24online/renew-package', [OnlineRenewSubscription::class, 'RenewSubscription']);
-Route::post('24online/user-status', [OnlineRenewSubscription::class, 'UserStatus']);
-Route::post('24online/renewal-history', [OnlineRenewSubscription::class, 'RenewalHistory']);
-Route::post('24online/user-password', [OnlineRenewSubscription::class, 'getUserPassword']);
-Route::post('24online/user-usage-info', [OnlineRenewSubscription::class, 'getUserUsageInfo']);
-Route::post('24online/payment-status', [OnlineRenewSubscription::class, 'getPaymentStatus']);
-Route::post('24online/invoice-detail', [OnlineRenewSubscription::class, 'getInvoiceDetail']);
-Route::post('24online/session-usage-detail', [OnlineRenewSubscription::class, 'sessionUsageDetails']);
-Route::post('24online/user-account-status', [OnlineRenewSubscription::class, 'getUserAccountStatus']);
+// Route::post('24online/renew-package', [OnlineRenewSubscription::class, 'RenewSubscription']);
+// Route::post('24online/user-status', [OnlineRenewSubscription::class, 'UserStatus']);
+// Route::post('24online/renewal-history', [OnlineRenewSubscription::class, 'RenewalHistory']);
+// Route::post('24online/user-password', [OnlineRenewSubscription::class, 'getUserPassword']);
+// Route::post('24online/user-usage-info', [OnlineRenewSubscription::class, 'getUserUsageInfo']);
+// Route::post('24online/payment-status', [OnlineRenewSubscription::class, 'getPaymentStatus']);
+// Route::post('24online/invoice-detail', [OnlineRenewSubscription::class, 'getInvoiceDetail']);
+// Route::post('24online/session-usage-detail', [OnlineRenewSubscription::class, 'sessionUsageDetails']);
+// Route::post('24online/user-account-status', [OnlineRenewSubscription::class, 'getUserAccountStatus']);
+
+// Route::post('/reminder/schedule', [ReminderController::class, 'schedulePaymentReminder']);
+// Route::get('/reminder/user-reminder', [ReminderController::class, 'getPaymentReminders']);
+// Route::post('/reminder/cancel', [ReminderController::class, 'cancelPaymentReminder']);
+// //FCMB
+// Route::post('fcmbank/account/create', [FcmbWalletController::class, 'createUserBankWalletAccount']);
 
 
-Route::middleware('auth:api')->group( function () {
-    Route::resource('products', ProductController::class);
-    Route::post('/users/send-registration-email', [RegisterController::class, 'sendRegistrationOTP']);
-});
-
-Route::post('/reminder/schedule', [ReminderController::class, 'schedulePaymentReminder']);
-Route::get('/reminder/user-reminder', [ReminderController::class, 'getPaymentReminders']);
-Route::post('/reminder/cancel', [ReminderController::class, 'cancelPaymentReminder']);
-//FCMB
-Route::post('fcmbank/account/create', [FcmbWalletController::class, 'createUserBankWalletAccount']);
+// Route::middleware('auth:api')->group( function () {
+//     Route::resource('products', ProductController::class);
+//     Route::post('/users/send-registration-email', [RegisterController::class, 'sendRegistrationOTP']);
+// });
